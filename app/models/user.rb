a840bacb99e08e20069147b569cb4d9216044ef5
Kirillshershen <151515833+Kirillshershen@users.noreply.github.com>
2025-06-16 15:45:38 +0300

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,24 @@
 class User < ApplicationRecord
-before_create :generate_telegram_link_token
-   def admin?
+  before_create :generate_telegram_link_token
+
+  def admin?
     admin
   end
 
+  def regenerate_telegram_link_token!
+    update!(telegram_link_token: SecureRandom.hex(10))
+  end
 
-def regenerate_telegram_link_token!
-  update!(telegram_link_token: SecureRandom.hex(10))
-end
+  private
+
+  def generate_telegram_link_token
+    self.telegram_link_token ||= SecureRandom.hex(10)
+  end
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-    has_many :orders, dependent: :destroy
+  has_many :orders, dependent: :destroy
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end

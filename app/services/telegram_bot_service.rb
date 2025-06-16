9af45784f9cc3c99d0e40d9e8bf17e949a790f6c
Kirillshershen <151515833+Kirillshershen@users.noreply.github.com>
2025-06-16 19:13:14 +0300

    1

--- app/services/telegram_bot_service.rb
+++ app/services/telegram_bot_service.rb
@@ -1,46 +1,25 @@
-  # app/services/telegram_bot_service.rb
-  require 'telegram/bot'
+# app/services/telegram_bot_service.rb
+require 'telegram/bot'
 
-  class TelegramBotService
-    @bot_client = nil
-
-    def self.bot_client
-      @bot_client ||= Telegram::Bot::Client.new(ENV['TELEGRAM_BOT_TOKEN'])
-    end
-
-    def self.run
-  bot_client.listen do |message|
-        case message
-        when Telegram::Bot::Types::Message
-          handle_message(bot_client, message)
-        end
-      end
-    end
-
-    def self.send_message(chat_id, text)
-      bot_client.api.send_message(chat_id: chat_id, text: text)
-    end
-    
+class TelegramBotService
   def self.handle_message(bot, message)
-        puts "Received message: #{message.text.inspect} from chat_id=#{message.chat.id}"
+    puts "Received message: #{message.text.inspect} from chat_id=#{message.chat.id}"
     if message.text&.start_with?('/start')
       token = message.text.split(' ')[1]
       user = User.find_by(telegram_link_token: token)
 
       if user
         if user.telegram_chat_id == message.chat.id
-          bot.api.send_message(chat_id: message.chat.id, text: 'Вы уже подк123лючены к Telegram.')
+          bot.api.send_message(chat_id: message.chat.id, text: 'Вы уже подключены к Telegram.')
         else
           user.update(telegram_chat_id: message.chat.id)
-          bot.api.send_message(chat_id: message.chat.id, text: 'Привет! Tele1111gram успешно подключён.')
-
+          bot.api.send_message(chat_id: message.chat.id, text: 'Привет! Telegram успешно подключён.')
         end
       else
-        bot.api.send_message(chat_id: message.chat.id, text: '1Пользователь не найден.')
+        bot.api.send_message(chat_id: message.chat.id, text: 'Пользователь не найден.')
       end
     else
-      bot.api.send_message(chat_id: message.chat.id, text: 'Не1известная команда.')
+      bot.api.send_message(chat_id: message.chat.id, text: 'Неизвестная команда.')
     end
   end
-
-  end
+end
--- bin/telegram_bot_runner.rb
+++ bin/telegram_bot_runner.rb
@@ -1,4 +1,8 @@
 #!/usr/bin/env ruby
 require_relative '../config/environment'
 
-TelegramBotService.run
+Telegram::Bot::Client.run(ENV['TELEGRAM_BOT_TOKEN'], logger: Rails.logger) do |bot|
+  bot.listen do |message|
+    TelegramBotService.handle_message(bot, message)
+  end
+end

22deff51ae8be1ce7ef04b597df7cc179e331668
Kirillshershen <151515833+Kirillshershen@users.noreply.github.com>
2025-06-16 16:09:10 +0300

    Update telegram_controller.rb

--- app/controllers/telegram_controller.rb
+++ app/controllers/telegram_controller.rb
@@ -3,35 +3,39 @@ skip_before_action :verify_authenticity_token, only: [:webhook]
 
 
   def webhook
-      Rails.logger.info "Telegram обновлениеууууууууууууу❌❌❌❌❌❌❌❌❌❌❌❌❌❌❌ууууууууууу: #{params.to_unsafe_h.inspect}"
+  update = params.to_unsafe_h  # или params[:message] - зависит от структуры
+  message = update['message'] || update[:message]
 
-    update = params.to_unsafe_h
+  if message && message['text'].present?
+    text = message['text']
+    chat_id = message.dig('chat', 'id')
 
-message = Telegram::Bot::Types::Message.new(params[:message])
-
-    return head :ok unless message.present?
-
-    chat_id = message["chat"]["id"]
-    text = message["text"]
-
-    if text&.start_with?("/start ")
-token = message.text.to_s.strip.split(' ')[1]
+    if text.start_with?('/start')
+      token = text.split(' ')[1]
       user = User.find_by(telegram_link_token: token)
+
       if user
-        user.update(telegram_chat_id: chat_id)
-        send_message(chat_id, "✅ Telegram привязан к вашему аккаунту.")
+        if user.telegram_chat_id == chat_id
+          send_message(chat_id, 'Вы уже подключены к Telegram.')
+        else
+          user.update(telegram_chat_id: chat_id)
+          send_message(chat_id, 'Привет! Telegram успешно подключён.')
+        end
       else
-        send_message(chat_id, "❌ Ошибка: токен не найден.")
+        send_message(chat_id, 'Пользователь не найден.')
       end
     else
-      send_message(chat_id, "Привет! Отправьте /start <токен> для привязки.")
+      send_message(chat_id, 'Неизвестная команда.')
     end
-
-    head :ok
-  rescue => e
-    Rails.logger.error "❌❌❌❌❌❌❌Telegram ошибка: #{e.class} — #{e.message}"
-    head :internal_server_error
   end
+
+  head :ok
+end
+
+def send_message(chat_id, text)
+  TelegramBotService.bot_client.api.send_message(chat_id: chat_id, text: text)
+end
+
 
   private
 

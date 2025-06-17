3184ffb62e2df9bbd05786a2c614c1a8c90424e7
Kirillshershen <151515833+Kirillshershen@users.noreply.github.com>
2025-06-17 22:49:27 +0300

    сделал карявое сохранение

--- app/models/quantity_promotion.rb
+++ app/models/quantity_promotion.rb
@@ -1,6 +1,5 @@
 class QuantityPromotion < ApplicationRecord
   belongs_to :promotion
 
-  validates :min_quantity, numericality: { greater_than: 0 }
-  validates :discount_value, numericality: { greater_than_or_equal_to: 0 }
+
 end
--- db/migrate/20250617112604_create_quantity_promotions.rb
+++ db/migrate/20250617112604_create_quantity_promotions.rb
@@ -2,8 +2,8 @@ class CreateQuantityPromotions < ActiveRecord::Migration[8.0]
   def change
     create_table :quantity_promotions do |t|
       t.references :promotion, null: false, foreign_key: true
-      t.integer :min_quantity, null: false
-      t.decimal :discount_value, null: false
+      t.integer :min_quantity, 
+      t.decimal :discount_value, 
 
       t.timestamps
     end
